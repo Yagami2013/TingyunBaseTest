08e66ff83b5629d2b1c1351b587c2f770099c10d
yangtt <yangtt@tingyun.com>
2020-04-02 16:55:11 +0800

    add log for service;add package name on service process

--- app/src/main/java/com/ytt/tingyunbasetest/service/NetRequest.java
+++ app/src/main/java/com/ytt/tingyunbasetest/service/NetRequest.java
@@ -4,12 +4,44 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-import androidx.annotation.Nullable;
+
+import com.ytt.tingyunbasetest.databuilder.Trace;
+import com.ytt.tingyunbasetest.util.LogY;
+
 
 public class NetRequest extends Service {
-    @Nullable
+    LogY logger=new LogY("Service");
+    IBinder mBinder;
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        logger.msg("service onBind");
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        logger.msg("service onCreate");
+        super.onCreate();
+        mBinder=new PlayerBinder();
+        Trace.all();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        logger.msg("service onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        logger.msg("service onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        logger.msg("service onDestroy");
     }
 }
